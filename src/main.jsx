54f7ca68cf82e8b8e34623eee0da984b9e8a2f4c
Suraj <suraj_bhandari@tsumos.com>
2024-12-23 21:44:55 +0900

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <img src="src/assets/react-logo.png" alt="React Logo" />
+    <h1>Hello World</h1>
+  </>
+);
